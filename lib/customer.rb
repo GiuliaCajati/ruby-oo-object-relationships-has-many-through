d3c0bbbe1f70189f3948b5adea9ffb23377a6350
giuliacajati <giuliacajati@gmail.com>
2020-08-31 08:33:00 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,41 @@
+#require 'pry'
+
 class Customer
-  
+    #has one waiter 
+    #has one meal 
+    attr_accessor :name, :age 
+
+    @@all = []
+
+    def initialize (name, age)
+        @name = name 
+        @age = age 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end
+
+    def meals 
+        #returns an Array of Meal instances associated with this customer 
+        Meal.all.select do |meal| #scan meal array 
+            meal.customer == self #
+        end 
+    end 
+
+    def new_meal (waiter, total, tip)
+        #initializes a meal using the current Customer instance, a provided Waiter instance and a total and tip
+        Meal.new(waiter, self, total, tip)   
+    end 
+   
+    def waiters
+        #returns an Array of Waiter instances associated with this customer's meals
+        meals.map do |meal| 
+            meal.waiter
+        end 
+    end 
+
 end
+
+#binding.pry
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,45 @@
-class Waiter
+#require 'pry'
 
+class Waiter
+    #has many customers 
+    #has many meals 
+
+    attr_accessor :name, :years_experience
+
+    @@all = []
+
+    def initialize (name, years_experience)
+        @name = name 
+        @years_experience = years_experience 
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def new_meal (customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def meals
+        #returns an Array of Meal instances associated with this waiter
+        Meal.all.select do |meal|
+            meal.waiter == self  
+    end
+    end 
+
+    def best_tipper
+        #returns the Customer instance associated with the meal that received the largest tip 
+        largest_tip = meals.max do |meal_1, meal_2|
+        meal_1.tip <=> meal_2.tip
+        # spaceship = 1, 0, or −1 depending on the value of the left
+    end 
+    largest_tip.customer 
+    end 
+    
 end
+
+
+
+#binding.pry
